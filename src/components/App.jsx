8b00077454f1c30bd87259d79140a27bea22c3c4
NZAlona <amakoveychenko@gmail.com>
2024-02-08 18:23:14 +0200

    Adds first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,20 @@
 
-// import { Product } from "./Product"
-import './App.css'
+import { Profile } from "./Profile/Profile";
+import userData from "../userData.json";
+import  "../index.css"
 
 
-
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Best selling</h1>
-
-//       <Product
-//         name="Tacos With Lime"
-//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-//         price={10.99}
-//       />
-//       <Product
-//         name="Fries and Burger"
-//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-//         price={14.29}
-//       />
-//     </div>
-//   );
-// }
-
-  const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" }
-];
-
-
-const BookList = ({ books }) => {
-  return (
-		<ul>
-			{books.map((book) => {
-		return <li key={book.id}>{book.name}</li>;
-			})}
-		</ul>
-  );
-}
-
-export const App =() => {
+export const App = () => {
   return (
     <>
-			<h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
+      <Profile
+        name={userData.username}
+        tag={userData.tag} 
+        location={userData.location}
+        image={userData.image}
+        stats={userData.stats}
+      />
     </>
   );
-}
-
-// Оголошення
-export const Card = ({ children }) => {
-  return <div>{children}</div>;
 };
-
-
 
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import css from "./Profile.module.css";
+
+export const Profile = ({name, location, tag, image, stats : {followers, views, likes}}) => {
+    return (
+        <div className={css.container}>
+<div className={css.imgWrapper}>
+<img src={image} alt="User avatar" className={css.avatar} />
+                <p className={css.userName}>{ name}</p>
+                <p className={css.userTag}>@{ tag}</p>
+                <p className={css.userLocation}>{ location}</p>
+</div>
+<ul className={css.list}>
+<li className={css.items}>
+<span className={css.followers}>Followers</span>
+<span className={css.userFollowers}>{ followers}</span>
+</li>
+<li className={css.items}>
+<span className={css.followers}>Views</span>
+<span className={css.userFollowers}>{views}</span>
+</li>
+<li className={css.items}>
+<span className={css.followers}>Likes</span>
+<span className={css.userFollowers}>{ likes}</span>
+</li>
+</ul>
+</div>
+
+    )
+}
